c231e9397b94d7197c957d9bea126ec964a05898
Gorka Navarrete <gorkang@gmail.com>
2021-04-11 18:54:03 +0100

    add data.table

--- _targets.R
+++ _targets.R
@@ -12,13 +12,13 @@
 
 
 # Set options, load packages -----------------------------------------------
-  
+
   # Source all /R files
   lapply(list.files("./R", full.names = TRUE, pattern = ".R"), source)
   
   # Packages to load
   main_packages = c("cli", "crayon", "furrr", "patchwork", "renv", "tarchetypes", "targets", "testthat")
-  data_preparation_packages = c("dplyr", "forcats", "here", "janitor", "purrr", "readr", "stringr", "tibble", "tidyr") #"safer", 
+  data_preparation_packages = c("data.table", "dplyr", "forcats", "here", "janitor", "purrr", "readr", "stringr", "tibble", "tidyr") #"safer", 
   data_analysis_packages = c("broom", "broom.mixed", "DT", "emmeans", "gmodels", "gt", "gtsummary", "irr", "kableExtra", "lme4", "parameters", "performance", "psych", "sjPlot", "skimr") #"report"
   data_visualization_packages = c("ggalluvial", "ggridges")
   non_declared_dependencies = c("qs", "visNetwork", "webshot", "performance", "bs4Dash", "pingr", "shiny", "shinycssloaders")
@@ -35,7 +35,7 @@
   
 # Declare pipeline --------------------------------------------------------
 
-  source("targets/targets_main.R")
-  # source("targets/targets_report.R")
+  # source("targets/targets_main.R")
+  source("targets/targets_report.R")
   
   targets
