015395fe2684ea8f86a4bf5d227c25acf5fb418a
Gorka Navarrete <gorkang@gmail.com>
2021-04-13 12:26:30 +0100

    improve setup

--- setup.R
+++ setup.R
@@ -1,38 +1,48 @@
+# Run this script to install all the dependencies needed.
 
-# Manual install ----------------------------------------------------------
-
-  # The targets and tarchetypes are not yet in CRAN. 
-  if (!require('remotes')) install.packages('remotes'); library('remotes')
-  if (!require('targets')) install.packages('targets'); library('targets')
-  if (!require('tarchetypes')) install.packages('tarchetypes'); library('tarchetypes')
-  remotes::install_github("gadenbuie/shrtcts")
+# You might need to run it twice and restart the RStudio session afterwards: Control + Shift + F10
 
 
-  
+# Install targets and tarchetypes ------------------------------------------
+
+if (!require('targets')) install.packages('targets'); library('targets')
+if (!require('tarchetypes')) install.packages('tarchetypes'); library('tarchetypes')
+
+
 # Make sure all packages are present --------------------------------------
 
-  if (!require('rmarkdown')) install.packages('rmarkdown'); library('rmarkdown')
-  
-  source("_targets.R")
-  missing_packages = packages_to_load[!packages_to_load %in% installed.packages()[,1]]
-  
-  if (length(missing_packages) > 0) {
-    cat("The following packages are missing and will be installed: ", packages_to_load[!packages_to_load %in% installed.packages()[,1]])
-    install.packages(packages_to_load[!packages_to_load %in% installed.packages()[,1]])
-  } else {
-    cat(crayon::green("All the necessary packages are present\n"))
-  }
+if (!require('rmarkdown')) install.packages('rmarkdown'); library('rmarkdown')
+
+source("_targets.R")
+missing_packages = packages_to_load[!packages_to_load %in% installed.packages()[,1]]
+
+if (length(missing_packages) > 0) {
+  cat("The following packages are missing and will be installed: ", packages_to_load[!packages_to_load %in% installed.packages()[,1]])
+  install.packages(packages_to_load[!packages_to_load %in% installed.packages()[,1]])
+} else {
+  cat(crayon::green("All the necessary packages are present\n"))
+}
+
+# If you have issues with DT::datables()
+# if (webshot::is_phantomjs_installed() == FALSE) webshot::install_phantomjs()
 
 
-  # Make sure all the necessary folders exist -----------------------------
-  
-  necessary_folders = c("data", "output/data", "output/plots", "output/reliability", "output/tables", "output/tests_outputs", ".vault", ".vault/output/data", ".vault/data_vault_5", ".vault/data_vault_6")
-  if (all(necessary_folders %in% dir(recursive = TRUE, include.dirs = TRUE, all.files = TRUE))) {
-    cat(crayon::green("All the necessary folders are present\n"))
-  } else {
-    cat(crayon::yellow("Creating necessary folders: "), paste(necessary_folders, collapse = ", "), "\n")
-    invisible(purrr::map(necessary_folders, dir.create, recursive = TRUE, showWarnings = FALSE))
-  }
+# Make sure all the necessary folders exist -----------------------------
 
-  # if (webshot::is_phantomjs_installed() == FALSE) webshot::install_phantomjs()
-  
+necessary_folders = c("data", "output/data", "output/plots", "output/reliability", "output/tables", "output/tests_outputs", ".vault", ".vault/output/data", ".vault/data_vault_5", ".vault/data_vault_6")
+if (all(necessary_folders %in% dir(recursive = TRUE, include.dirs = TRUE, all.files = TRUE))) {
+  cat(crayon::green("All the necessary folders are present\n"))
+} else {
+  cat(crayon::yellow("Creating necessary folders: "), paste(necessary_folders, collapse = ", "), "\n")
+  invisible(purrr::map(necessary_folders, dir.create, recursive = TRUE, showWarnings = FALSE))
+}
+
+
+# Configure shourcuts ---------------------------------------------------
+
+# Will install the shrtcts package from Github and copy the config files to your home folder
+
+# Control+P: load all packages and functions
+# Control+M: targets::tar_make()
+# Control+L: targets::rstudio_addin_tar_load()
+source("R/setup_shortcuts.R"); setup_shortcuts()
